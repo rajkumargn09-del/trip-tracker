93444d374cea6e397529ccf24513c2ce80a0ee9a
rajkumargn9 <rajkumargn09@gmail.com>
2026-02-13 17:26:50 +0530

    Allow database path customization via DATABASE_PATH

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'expenses.db');
+const dbPath = process.env.DATABASE_PATH || path.resolve(__dirname, 'expenses.db');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Could not connect to database', err);
